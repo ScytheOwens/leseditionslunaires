831ab7410812b04b4c7b6d4be636f403b93eec9c
Maëva GRONDIN <maeva.grondin@idci-consulting.fr>
2024-10-19 01:00:31 +0200

    Notion-16 Add product model

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Repository;
+
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -9,5 +11,13 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    public function getProducts() 
+    {
+        $dql = "SELECT * FROM product";
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        return $query->getResult();
     }
 }
