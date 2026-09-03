1c25778a895f46155bef8f173b140b97266eed89
Maëva GRONDIN <maeva.grondin@idci-consulting.fr>
2026-09-04 00:31:56 +0200

    Notion-9 Add cgv

---
+++ src/Controller/StaticController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'static_')]
+class StaticController extends AbstractController
+{
+    public const STATIC_PAGE_SLUG_TEMPLATE_MAPPING = [
+        'cgv' => 'general_terms',
+    ];
+
+    #[Route('/{slug}', name: 'show')]
+    public function show(string $slug): Response
+    {
+        $templateFilename = self::STATIC_PAGE_SLUG_TEMPLATE_MAPPING[$slug];
+
+        if (empty($templateFilename)) {
+            throw $this->createNotFoundException('Cette page n\'existe pas.');
+        }
+
+        return $this->render(sprintf('static/%s.html.twig', $templateFilename));
+    }
+}
